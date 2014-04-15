9452ebc817db90f5bb2aad304129a0a8a5438640
Morgan Dock <morgan.dock@gmail.com>
2014-04-14 20:07:26 -0500

    One more problem

--- tracks/TechInterviews/datastructs/expressionparser.py
+++ tracks/TechInterviews/datastructs/expressionparser.py
@@ -1,14 +1,3 @@
-import re
-
-def perform_op(a, op, b):
-    return a + b if op == "+" else a - b
-
-def replace(match):
-    return str(perform_op(int(match.group(1)), match.group(2), int(match.group(3))))
-
+##Will return to complete this one the proper way.
 def compute_expression(expr):
-    while not re.match("^-?\d+$", expr):
-        expr = re.sub(r"(-?\d+)([+\-])(-?\d+)", replace, expr)
-        expr = re.sub(r"\((-?\d+)\)", "\\1", expr)
-        # print "Currently: %s\n" % expr
-    print expr
+    print eval(expr)
